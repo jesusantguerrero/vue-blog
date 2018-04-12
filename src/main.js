98a9ead6cfffc7e2da5883bc7587ee6432d3868c
Jesus Guerrero <jesusant@mctekk.com>
2018-04-12 17:10:55 -0400

    add json server

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import mixin from './utils/mixin';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import VueFroala from './froala';
+
+axios.defaults.baseURL = 'http://localhost:3600';
+
 Vue.use(VueFroala);
+Vue.use(VueAxios, axios);
+
+Vue.mixin(mixin)
 
 export default new Vue({
-    router,
+		router,
     render: h => h(App)
 }).$mount('#app');
